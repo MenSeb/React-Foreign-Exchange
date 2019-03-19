de0ede3094253e463ae55ee4fa12a5c41eab476e
MenSeb <sebastien.menard.github@gmail.com>
2019-03-19 14:45:55 -0400

    update builders index

---
+++ src/builders/index.js
@@ -0,0 +1,11 @@
+import Button from './Button';
+import Field from './Field';
+import FieldControl from './FieldControl';
+import Icon from './Icon';
+
+export {
+  Button,
+  Field,
+  FieldControl,
+  Icon,
+}

679c19e7be3022bd25d5bf1d73e97f7c9db9c76f
MenSeb <sebastien.menard.github@gmail.com>
2019-03-19 15:50:13 -0400

    moving PropTypes to Input builder

--- src/hooks/input/input.js
+++ src/hooks/input/input.js
@@ -1,5 +1,4 @@
 import { useState, useCallback } from 'react'
-import PropTypes from 'prop-types'
 
 export default function useInput ({ validation, initialValue = '' })
 {
@@ -20,15 +19,4 @@ export default function useInput ({ validation, initialValue = '' })
   )
 
   return { value, changeValue }
-}
-
-useInput.propTypes = {
-  validation: ( props, name, source ) =>
-  {
-    if ( !props[ name ] instanceof RegExp )
-      return new Error(
-        `Invalid validation from ${ source }. Should be a RegExp.`
-      )
-  },
-  initialValue: PropTypes.string,
 }

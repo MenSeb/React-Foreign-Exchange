ef34cfaca9c50573f1c1336754b01a4f297c1372
MenSeb <sebastien.menard.github@gmail.com>
2019-03-25 18:24:56 -0400

    update App structure with components

--- src/App/App.js
+++ src/App/App.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import { ForeignExchangeProvider } from '../contexts/foreign-exchange'
+import SvgDefs from '../builders'
 
 export default function App ()
 {
   return (
     <div className='App'>
+
+      <SvgDefs />
+
+      <ForeignExchangeProvider />
 
     </div>
   )

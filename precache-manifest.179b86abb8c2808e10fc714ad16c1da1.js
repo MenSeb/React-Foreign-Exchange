e074eb9cf4fb67309aeecec94f671ade1cdeb75e
MenSeb <sebastien.menard.github@gmail.com>
2019-04-07 23:04:51 -0400

    Updates

---
+++ precache-manifest.179b86abb8c2808e10fc714ad16c1da1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3e757feaf9f43618b250",
+    "url": "/react-foreign-exchange/static/css/main.98b9b313.chunk.css"
+  },
+  {
+    "revision": "3e757feaf9f43618b250",
+    "url": "/react-foreign-exchange/static/js/main.893e28fb.chunk.js"
+  },
+  {
+    "revision": "9383bae7603ccbb3989e",
+    "url": "/react-foreign-exchange/static/js/runtime~main.43b31163.js"
+  },
+  {
+    "revision": "377862c88619ec3576c7",
+    "url": "/react-foreign-exchange/static/js/2.6d78373f.chunk.js"
+  },
+  {
+    "revision": "a7a19b4c419be3a75b8d4929f9178b4a",
+    "url": "/react-foreign-exchange/index.html"
+  }
+];
